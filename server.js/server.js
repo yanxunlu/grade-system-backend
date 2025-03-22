83ea53806121c7b20ed555d824587189c43915e0
yanxunlu <luyanxun06@gmail.com>
2025-03-22 23:40:48 +0800

    Update server.js

--- server.js/server.js
+++ server.js/server.js
@@ -265,6 +265,9 @@ app.get("/grades/subjectDistribution", async (req, res) => {
 });
 
 // 啟動伺服器
-app.listen(port, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 伺服器運行於 http://localhost:${port}`);
 });
+
